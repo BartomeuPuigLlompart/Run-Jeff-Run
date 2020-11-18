8d6a52f176cdd5f5785403f41aa11e4bb46764ee
Bartomeu <bartomeu.puig@enti.cat>
2020-11-18 20:22:17 +0100

    Suelo + movimiento cámara

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    enum states { DEAD, HURT, NORMAL}
+    [SerializeField] states state;
+    [SerializeField]Transform gameMidRef;
+    [SerializeField] float speed;
+    const float deadRef = 0.015f;
+    const float hurtRef = 0.02f;
+    const float normalRef = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case states.NORMAL:
+                speed = speed + (normalRef - speed) * 0.01f;
+                break;
+            case states.HURT:
+                speed = hurtRef;
+                break;
+            case states.DEAD:
+                speed = deadRef;
+                break;
+        }
+        transform.position = Vector2.Lerp(transform.position, new Vector2(gameMidRef.position.x, transform.position.y), speed);
+    }
+
+    //void hurtplayer()
+    //{
+    //    switch (state)
+    //    {
+    //        case states.normal:
+    //            speed = normalref;
+    //            break;
+    //        case states.hurt:
+    //            speed = hurtref;
+    //            break;
+    //        case states.dead:
+    //            speed = deadref;
+    //            break;
+    //    }
+    //}
+}
---
+++ Assets/Scripts/Procedural/RenderManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderManager : MonoBehaviour
+{
+    float groundfOffset = 21.6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Render Objects")
+        {
+            if (collision.gameObject.layer == 8) 
+                SpawnGround(collision.transform);
+            else Destroy(collision.gameObject);
+        }
+
+    }
+
+    void SpawnGround(Transform groundObject)
+    {
+        groundObject.transform.position += new Vector3(groundfOffset * 2.0f, 0, 0);
+    }
+}
