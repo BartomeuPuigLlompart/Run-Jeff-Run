e36aea1f5ab1a71461bbe267bf09d5d22d8a2951
Jordi Silvestrini Hernández <jordi.silvestrini@enti.cat>
2020-11-20 17:36:05 +0100

    Start Jump and Platform

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    [Range(1, 20)]
+    [SerializeField] private float jumpVelocity = 5f;
+    //[SerializeField] private float fallMultiplier = 2.5f;
+    //[SerializeField] private float lowJumpMultiplier = 2f;
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
+        }
+    }
+
+    //private void FixedUpdate()
+    //{
+    //    if(rb.velocity.y < 0)
+    //    {
+    //        rb.gravityScale = fallMultiplier;
+    //    }
+    //    else if(rb.velocity.y > 0 && !Input.GetButton("Jump"))
+    //    {
+    //        rb.gravityScale = lowJumpMultiplier;
+    //    }
+    //    else
+    //    {
+    //        rb.gravityScale = 1f;
+    //    }
+    //}
+}
---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    private PlatformEffector2D effector;
+    [SerializeField] private float waitTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            waitTime = .1f;
+        }
+
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            if(waitTime <= 0)
+            {
+                effector.rotationalOffset = 180f;
+                waitTime = .1f;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+
+        if(Input.GetButton("Jump"))
+        {
+            effector.rotationalOffset = 0;
+        }
+    }
+}
