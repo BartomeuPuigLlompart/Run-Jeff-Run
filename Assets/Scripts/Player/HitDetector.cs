e908e6ab956e0cac6387265a6525895882af4a07
Bartomeu <bartomeu.puig@enti.cat>
2020-11-28 20:43:32 +0100

    spawn monedas + rate

--- Assets/Scripts/Player/HitDetector.cs
+++ Assets/Scripts/Player/HitDetector.cs
@@ -7,7 +7,8 @@ public class HitDetector : MonoBehaviour
     
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.tag != "Player" && collision.tag != "Camera Scope")
+        if (collision.tag != "Player" && collision.tag != "Camera Scope" &&
+            collision.gameObject.layer != 9)
             PlayerController.playerController.hurtplayer();
     }
 }
