c34a6ab67c3562fc00890b0107f06a97b21b7939
Jordi Silvestrini Hernández <jordi.silvestrini@enti.cat>
2020-12-10 22:14:18 +0100

    Update Input and Transitions

---
+++ Assets/Scripts/Procedural/MenuManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject firstUI;
+
+    [SerializeField] private GameObject menu;
+    [SerializeField] private GameObject levels;
+    [SerializeField] private GameObject shop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu.SetActive(false);
+        levels.SetActive(false);
+        shop.SetActive(false);
+
+        firstUI.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToMenu()
+    {
+        menu.SetActive(true);
+        levels.SetActive(false);
+        shop.SetActive(false);
+    }
+
+    public void GoToLevels()
+    {
+        levels.SetActive(true);
+        menu.SetActive(false);
+        shop.SetActive(false);
+    }
+
+    public void GoToShop()
+    {
+        shop.SetActive(true);
+        menu.SetActive(false);
+        levels.SetActive(false);
+    }
+}
