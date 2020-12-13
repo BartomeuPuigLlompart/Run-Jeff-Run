1a2c056a279efcbf830801492fc8d17bd8bef7eb
Jordi Silvestrini Hernández <jordi.silvestrini@enti.cat>
2020-12-13 20:50:37 +0100

    Update Splashscreen

---
+++ Assets/Scripts/Menu/Delay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Delay : MonoBehaviour
+{
+    [SerializeField] private float timeTot;
+
+    private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time >= timeTot) SceneManager.LoadScene(1);
+    }
+}
