97bfce4ba6fa08c05b111e217abb500c35f08a91
Bartomeu <bartomeu.puig@enti.cat>
2021-01-05 00:48:30 +0100

    Dependencias de app y base de login y database

---
+++ Assets/Scripts/App/SignInScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using Firebase.Auth;
+using Firebase;
+using Firebase.Database;
+//using Firebase.Unity.Editor;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SignInScript : MonoBehaviour
+{
+    FirebaseAuth firebaseAuth;
+    FirebaseDatabase _database;
+    DatabaseReference reference;
+
+    // Use this for initialization
+    void Start()
+    {
+        firebaseAuth = Firebase.Auth.FirebaseAuth.DefaultInstance; ;
+        //FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://runjeffrun-3949c-default-rtdb.europe-west1.firebasedatabase.app/");
+        //DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+        _database = FirebaseDatabase.GetInstance("https://runjeffrun-3949c-default-rtdb.europe-west1.firebasedatabase.app/");
+        reference = _database.RootReference;
+        
+        Invoke("SignIn", 2);
+    }
+
+
+    public void SignIn()
+    {
+        //firebaseAuth.CreateUserWithEmailAndPasswordAsync("tpuigllompart@gmail.com", "123456").ContinueWith(task =>
+        //{
+        //    if (task.IsCanceled)
+        //    {
+        //        Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+        //        return;
+        //    }
+        //    if (task.IsFaulted)
+        //    {
+        //        Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+        //        return;
+        //    }
+
+        //    // Firebase user has been created.
+        //    Firebase.Auth.FirebaseUser newUser = task.Result;
+        //    Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+        //        newUser.DisplayName, newUser.UserId);
+
+        //    writeNewUser(newUser.UserId, "tpuigllompart@gmail.com");
+        //});
+        writeNewUser("122", "tpuigllompart@gmail.com");
+    }
+
+    private void writeNewUser(string userId, string email)
+    {
+        User user = new User("Tutor", email, "none", userId, "none");
+        string json = JsonUtility.ToJson(user);
+        _database.GetReference("scores/").SetValueAsync(1).ContinueWith(task =>
+        {
+            if (task.IsFaulted) Debug.Log("F in the chat");
+            else if (task.IsCompleted)
+            {
+                Debug.Log("Ye");
+            }
+            
+        });
+        reference.Child("users").Child(userId).SetRawJsonValueAsync(json);
+    }
+
+}
---
+++ Assets/Scripts/App/User.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User
+{
+    public string rol;
+    public string correo;
+    public string correoOpuesto;
+    public string id;
+    public string idOpuesto;
+    public string usernameOpuesto;
+
+    public User()
+    {
+    }
+    public User(string _rol, string _correo, string _correoOpuesto, string _id, string _idOpuesto, string _usernameOpuesto = "Tutor")
+    {
+        rol = _rol;
+        correo = _correo;
+        correoOpuesto = _correoOpuesto;
+        id = _id;
+        idOpuesto = _idOpuesto;
+        usernameOpuesto = _usernameOpuesto;
+    }
+}
