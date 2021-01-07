a2eb478ecd374165ba767cd804f3e9ffdc6d1cea
Bartomeu <bartomeu.puig@enti.cat>
2021-01-07 20:40:00 +0100

    mejoras en registro de usuario

--- Assets/Scripts/App/SignInScript.cs
+++ Assets/Scripts/App/SignInScript.cs
@@ -11,62 +11,116 @@ using UnityEngine.SceneManagement;
 
 public class SignInScript : MonoBehaviour
 {
+    const string glyphs = "abcdefghijklmnopqrstuvwxyz0123456789";
+
     FirebaseAuth firebaseAuth;
     FirebaseDatabase _database;
     DatabaseReference reference;
 
+    [SerializeField] Text email, playerEmail;
+    [SerializeField] InputField password, passwordConfirmation;
+
+
     // Use this for initialization
     void Start()
     {
+        email.horizontalOverflow = HorizontalWrapMode.Wrap;
         firebaseAuth = Firebase.Auth.FirebaseAuth.DefaultInstance; ;
-        //FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://runjeffrun-3949c-default-rtdb.europe-west1.firebasedatabase.app/");
-        //DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
         _database = FirebaseDatabase.GetInstance("https://runjeffrun-3949c-default-rtdb.europe-west1.firebasedatabase.app/");
         reference = _database.RootReference;
-        
-        Invoke("SignIn", 2);
     }
 
 
     public void SignIn()
     {
-        //firebaseAuth.CreateUserWithEmailAndPasswordAsync("tpuigllompart@gmail.com", "123456").ContinueWith(task =>
-        //{
-        //    if (task.IsCanceled)
-        //    {
-        //        Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
-        //        return;
-        //    }
-        //    if (task.IsFaulted)
-        //    {
-        //        Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
-        //        return;
-        //    }
+        if(password.text != passwordConfirmation.text)
+        {
+            Debug.LogError("You entered two different passwords");
+            return;
+        }
 
-        //    // Firebase user has been created.
-        //    Firebase.Auth.FirebaseUser newUser = task.Result;
-        //    Debug.LogFormat("Firebase user created successfully: {0} ({1})",
-        //        newUser.DisplayName, newUser.UserId);
+        firebaseAuth.CreateUserWithEmailAndPasswordAsync(email.text, password.text).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
 
-        //    writeNewUser(newUser.UserId, "tpuigllompart@gmail.com");
-        //});
-        writeNewUser("122", "tpuigllompart@gmail.com");
+            // Firebase user has been created.
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+
+            writeNewUser(newUser.UserId, email.text);
+        });
     }
 
     private void writeNewUser(string userId, string email)
     {
         User user = new User("Tutor", email, "none", userId, "none");
         string json = JsonUtility.ToJson(user);
-        _database.GetReference("scores/").SetValueAsync(1).ContinueWith(task =>
+        reference.Child("users").Child(userId).SetRawJsonValueAsync(json).ContinueWith(task =>
+        {
+            if (task.IsFaulted) Debug.Log("F in the chat");
+            else if (task.IsCompleted)
+            {
+                PlayerPrefs.SetString("email", email);
+                PlayerPrefs.SetString("userId", userId);
+            }
+            
+        });
+    }
+
+    public void setPlayer()
+    {
+        string randomPassword = "";
+
+        for (int i = 0; i < 15; i++)
+        {
+            randomPassword += glyphs[Random.Range(0, glyphs.Length)];
+        }
+
+        firebaseAuth.CreateUserWithEmailAndPasswordAsync(playerEmail.text, randomPassword).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+
+            writeNewPlayer(newUser.UserId, playerEmail.text);
+        });
+    }
+
+    private void writeNewPlayer(string userId, string email)
+    {
+        User user = new User("Tutor", email, "none", userId, "none");
+        string json = JsonUtility.ToJson(user);
+        reference.Child("users").Child(userId).SetRawJsonValueAsync(json).ContinueWith(task =>
         {
             if (task.IsFaulted) Debug.Log("F in the chat");
             else if (task.IsCompleted)
             {
                 Debug.Log("Ye");
             }
-            
+
         });
-        reference.Child("users").Child(userId).SetRawJsonValueAsync(json);
     }
 
 }
